1520148258e0a50462f22854d2951497c6b82969
Vithujan99 <vithujan99@hotmail.com>
2023-07-08 19:03:29 +0200

    Homepage RentCarousel finished

---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
+
+import "./Carousel.css";
+
+export const Carousel = ({ data }) => {
+  const [slide, setSlide] = useState(0);
+  const nextSlide = () => {
+    setSlide(slide === data.slides.length - 1 ? 0 : slide + 1);
+  };
+  const prevSlide = () => {
+    setSlide(slide === 0 ? data.slides.length - 1 : slide - 1);
+  };
+
+  return (
+    <div className="carousel">
+      <BsArrowLeftCircleFill className="arrow arrow-left" onClick={prevSlide} />
+      {data.slides.map((item, idx) => {
+        return (
+          <img
+            className={slide === idx ? "slide" : "slide slide-hidden"}
+            src={item.src}
+            alt={item.alt}
+            key={idx}
+          />
+        );
+      })}
+      <BsArrowRightCircleFill
+        className="arrow arrow-right "
+        onClick={nextSlide}
+      />
+      <span className="indicators">
+        {data.slides.map((_, idx) => {
+          return (
+            <button
+              className={
+                slide === idx ? "indicator" : "indicator indicator-inactive"
+              }
+              key={idx}
+              onClick={() => setSlide(idx)}
+            ></button>
+          );
+        })}
+      </span>
+    </div>
+  );
+};
--- src/pages/Home/RentContent/RentContent.js
+++ src/pages/Home/RentContent/RentContent.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { Carousel } from "../../../components/Carousel/Carousel";
+import data from "../../../data/carouselData.json";
 import "./RentContent.css";
 
 const RentContent = () => {
   return (
     <div class="rent-content">
       <div className="button-holder">
-        <Link className="rent-titel" to="/rustica-webpage/menu">
-          Raum Mieten
+        <Link className="rent-titel" to="/rustica-webpage/rent">
+          Raum Buchen
         </Link>
       </div>
+      <Carousel data={data} />
     </div>
   );
 };
