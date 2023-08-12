1300d71d35b05bdda9d7092b14cf53a14fa1c5d4
Vithujan99 <vithujan99@hotmail.com>
2023-08-12 12:58:59 +0200

    Zusatz loads faster

--- src/context/ShowContext.js
+++ src/context/ShowContext.js
@@ -4,40 +4,19 @@ export const ShowContext = createContext({
   showBar: false,
   handleBarClose: () => {},
   handleBarShow: () => {},
-  showZusatz: false,
-  zusatzName: "",
-  handleZusatzClose: () => {},
-  handleZusatzShow: () => {},
-  handleZusatzName: () => {},
 });
 export function ShowProvider({ children }) {
   const [showB, setShowB] = useState(false);
-  const [showZ, setShowZ] = useState(false);
-  const [zusatzN, setZusatzN] = useState();
   function handleBarClose() {
     setShowB(false);
   }
   function handleBarShow() {
     setShowB(true);
   }
-  function handleZusatzClose() {
-    setShowZ(false);
-  }
-  function handleZusatzShow() {
-    setShowZ(true);
-  }
-  function handleZusatzName(name) {
-    setZusatzN(name);
-  }
   const contextValue = {
     showBar: showB,
     handleBarClose,
     handleBarShow,
-    showZusatz: showZ,
-    zusatzName: zusatzN,
-    handleZusatzClose,
-    handleZusatzShow,
-    handleZusatzName,
   };
   return (
     <ShowContext.Provider value={contextValue}>{children}</ShowContext.Provider>
--- src/pages/Menu/MenuItems/MenuItem/MenuItem.js
+++ src/pages/Menu/MenuItems/MenuItem/MenuItem.js
@@ -1,16 +1,33 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { CartContext } from "../../../../context/CartContext";
-import { ShowContext } from "../../../../context/ShowContext";
 import { IngredientsContext } from "../../../../context/IngredientsContext";
 import { formatCurrency } from "../../../../utilities/formatCurrency";
+import {
+  getIngredientstData,
+  ingredientsData,
+} from "../../../../data/ingredientsData";
 
-import MenuZusatz from "../MenuZusatz/MenuZusatz";
 import "./MenuItem.css";
+import "./MenuZusatz.css";
 
 export const MenuItem = ({ data }) => {
   const cart = useContext(CartContext);
-  const show = useContext(ShowContext);
   const ingredients = useContext(IngredientsContext);
+  const [show, setShow] = useState(false);
+  function handleZusatzShow() {
+    setShow(true);
+  }
+  function handleZusatzClose() {
+    setShow(false);
+  }
+  const [zusatzName, setZusatzName] = useState();
+  function handleZusatzName(name) {
+    setZusatzName(name);
+  }
+  const [text, handleText] = useState("");
+  function setText(newText) {
+    handleText(newText.target.value);
+  }
 
   return (
     <div className="item-card">
@@ -29,15 +46,176 @@ export const MenuItem = ({ data }) => {
           if (data.zusatz === undefined) {
             cart.addOneToCart(data.id, undefined, undefined);
           } else {
-            show.handleZusatzShow();
+            handleZusatzShow();
             ingredients.handleItemId(data.id);
-            show.handleZusatzName(data.zusatz);
+            handleZusatzName(data.zusatz);
           }
         }}
       >
         Hinzufügen
       </button>
-      <MenuZusatz data={data} />
+      <div>
+        <div
+          className={
+            show ? "ask-zusatz-container" : "ask-zusatz-container hide"
+          }
+        >
+          <div
+            className="black-background"
+            onClick={() => handleZusatzClose()}
+          />
+          <div className="ask-zusatz-content">
+            <div className="ask-zusatz-head">
+              <div className="ask-zusatz-head-titel">
+                {("0" + data.id).slice(-3)}.{data.name}
+              </div>
+              <div
+                className="ask-zusatz-close"
+                onClick={() => handleZusatzClose()}
+              >
+                X
+              </div>
+              <div className="ask-zusatz-head-header">
+                <div className="ask-zusatz-head-header-summe">
+                  Summe{" "}
+                  <span>
+                    {formatCurrency(data.price + ingredients.getTotalCost())}
+                  </span>
+                </div>
+                <button
+                  className="ask-zusatz-head-item-kaufen"
+                  onClick={() => {
+                    cart.addOneToCart(
+                      data.id,
+                      ingredients.ingredientsArray,
+                      text
+                    );
+                    ingredients.removeItem(data.id);
+                    handleText("");
+                    handleZusatzClose();
+                  }}
+                >
+                  Hinzufügen
+                </button>
+              </div>
+              <div className="ask-zusatz-head-added-items">
+                {ingredients.ingredientsArray.length === 0 ? (
+                  <>Extras</>
+                ) : (
+                  <></>
+                )}
+                {ingredients.ingredientsArray.map((ingredient) => (
+                  <div className="ask-zusatz-head-added-item">
+                    <div
+                      onClick={() =>
+                        ingredients.removeIngredient(
+                          data.id,
+                          ingredient.ingredientId
+                        )
+                      }
+                      className="ask-zusatz-item-button"
+                    >
+                      -
+                    </div>
+                    <p>
+                      <b className="ask-zusatz-item-count">
+                        {ingredient.quantity}
+                      </b>
+                      {getIngredientstData(ingredient.ingredientId).name}
+                    </p>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="ask-zusatz-body">
+              {zusatzName === "Zusatz2" ? (
+                <div className="ask-zusatz-body-section">
+                  <label
+                    className="ask-zusatz-body-section-name"
+                    for="salatIng"
+                  >
+                    Salat Dressing
+                  </label>
+                  <br></br>
+                  <select
+                    className="ask-zusatz-body-section-select"
+                    onChange={(event) => {
+                      ingredients.addDressingIngredient(
+                        data.id,
+                        event.target.value
+                      );
+                    }}
+                    required
+                    name="salatIngredients"
+                    id="salatIng"
+                  >
+                    {ingredientsData.map((ingredientSection) =>
+                      ingredientSection.name === zusatzName ? (
+                        ingredientSection.items.map((ingredient) => (
+                          <option value={ingredient.id}>
+                            {ingredient.name}
+                          </option>
+                        ))
+                      ) : (
+                        <></>
+                      )
+                    )}
+                  </select>
+                </div>
+              ) : (
+                <>
+                  {ingredientsData.map((ingredientSection) =>
+                    ingredientSection.name === zusatzName ? (
+                      <div className="ask-zusatz-body-section">
+                        <p className="ask-zusatz-body-section-name">
+                          Zutaten für:{formatCurrency(ingredientSection.price)}
+                        </p>
+                        <div className="ask-zusatz-body-section-items">
+                          {ingredientSection.items.map((ingredient) => (
+                            <div className="ask-zusatz-body-section-item">
+                              <div
+                                onClick={() => {
+                                  ingredients.addIngredient(
+                                    data.id,
+                                    ingredient.id
+                                  );
+                                }}
+                                className="ask-zusatz-item-button"
+                              >
+                                +
+                              </div>
+                              <p>{ingredient.name}</p>
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                    ) : (
+                      <></>
+                    )
+                  )}
+                </>
+              )}
+              <div className="ask-zusatz-body-section-textfeld-container">
+                <label className="ask-zusatz-body-section-textfeld-beschreibung">
+                  {" "}
+                  Hinweis:{" "}
+                  <span>
+                    z.B. dunkel backen, ohne Oregano, schneiden, etc. ( Sonstige
+                    Extra-Zutaten werden bei der Lieferung/Abholung zusätzlich
+                    berechnet).
+                  </span>
+                </label>
+
+                <textarea
+                  className="ask-zusatz-body-section-textfeld"
+                  value={text}
+                  onChange={(event) => setText(event)}
+                ></textarea>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
