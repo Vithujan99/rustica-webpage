1c0502b32ffad2a9a1533d7f6e97ba20fd831359
Vithujan99 <vithujan99@hotmail.com>
2023-10-08 21:59:08 +0200

    Footer und Impressum

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -7,7 +7,7 @@ const verifyJWT = (req, res, next) => {
   const authHeader = req.headers["x-access-token"];
   const token = authHeader && authHeader.split(" ")[1];
   if (!token) {
-    res.send("Nicht Angemeldet");
+    res.json({ auth: false, message: "Nicht Angemeldet" });
   } else {
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
       if (err) {
